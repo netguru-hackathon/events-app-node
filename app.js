cdbade8d8b1d6b6e0d756f6da6119399a6205697
Artur Figiel <arturffigiel@gmail.com>
2017-05-02 20:46:59 +0200

    Basic structure of auth

--- app.js
+++ app.js
@@ -6,14 +6,51 @@ const request = require('request')
 const path = require('path')
 const app = express()
 
+const _ = require("lodash");
+const bodyParser = require("body-parser");
+const jwt = require('jsonwebtoken');
+
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+let jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
+jwtOptions.secretOrKey = 'turboKoala2000';
+
+var strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+  console.log('payload received', jwt_payload);
+  // database call:
+  var user = users[_.findIndex(users, {id: jwt_payload.id})];
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+
 app.get('/', (req, res) => {
-  console.log(req.query.log);
   res.send('Hello!')
 })
 
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname+'/log_button.html'))
 })
+
+app.use(passport.initialize());
+
+// parse application/x-www-form-urlencoded
+// for easier testing with Postman or plain HTML forms
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/slack/auth', (req, res) =>{
     var options = {
@@ -31,6 +68,9 @@ app.get('/slack/auth', (req, res) =>{
             res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
         }else{
             console.log(JSONresponse)
+            //sequelize init
+
+
             res.sendFile(path.join(__dirname+'/after_login.html'))
         }
     })
