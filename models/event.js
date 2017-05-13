723247d506b5041f80d6167140c0d9dfa6d2f87b
Artur Figiel <arturffigiel@gmail.com>
2017-05-13 16:46:52 +0200

    Add event - users associations

--- models/event.js
+++ models/event.js
@@ -7,7 +7,11 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        models.Event.belongsToMany(models.User, {
+          through: models.UserEvent,
+          as: 'event',
+          foreignKey: 'event_id'
+        })
       }
     }
   });
--- models/user.js
+++ models/user.js
@@ -7,7 +7,11 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        models.User.belongsToMany(models.Event, {
+          through: models.UserEvent,
+          as: 'user',
+          foreignKey: 'user_id'
+        })
       }
     }
   });
