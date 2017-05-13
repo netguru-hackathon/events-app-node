07fcaaad23443ee8ac605f09464ec8c4b781075f
Rafał Gawlik <rafal.maly@gmail.com>
2017-05-13 13:19:56 +0200

    Extract slack auth service

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const request = require('request')
 const _ = require("lodash")
 const passportJWT = require("passport-jwt");
 const ExtractJwt = passportJWT.ExtractJwt;
+import { authorizeWithAuthCode } from './services/slackAuth'
 
 const app = express();
 app.use(bodyParser.json())
@@ -38,34 +39,20 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/slack/auth', (req, res) =>{
-    var options = {
-        uri: 'https://slack.com/api/oauth.access?code='
-            +req.query.code+
-            '&client_id='+process.env.CLIENT_ID+
-            '&redirect_uri='+process.env.REDIRECT_URI,
-        method: 'GET'
-    }
-    return request(options, (error, response, body) => {
-        var JSONresponse = JSON.parse(body)
-        if (!JSONresponse.ok){
-            console.log(JSONresponse)
-            res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
-        } else {
-          let username = JSONresponse.user.name;
-          let slack_id = JSONresponse.user.id;
-          return models.User.findOrCreate({where: { name: username, slack_id: slack_id }})
-            .then((result) => {
-              let user = result[0]
-              let payload = {id: user.dataValues.id};
-              jwt.sign(payload, jwtOptions.secretOrKey, (err, token) => {
-                if(err) { console.error(err); }
+  authorizeWithAuthCode(req.query.code)
+    .then(({username, slack_id}) => {
+      return models.User.findOrCreate({where: { name: username, slack_id: slack_id }})
+        .then((result) => {
+          let user = result[0]
+          let payload = {id: user.dataValues.id};
+          jwt.sign(payload, jwtOptions.secretOrKey, (err, token) => {
+            if(err) { console.error(err); }
 
-                user.update({token: token}).then(function() {
-                  res.json({message: "ok", token: token})
-                })
-              })
+            user.update({token: token}).then(function() {
+              res.json({message: "ok", token: token})
             })
-        }
+          })
+        }).catch((error) => res.send(error))
     })
 })
 
---
+++ services/slackAuth.js
@@ -0,0 +1,33 @@
+'use strict'
+
+require('dotenv').config()
+const request = require('request')
+
+function authorizeWithAuthCode(authCode) {
+  return new Promise((resolve, reject) => {
+    const options = {
+        uri: 'https://slack.com/api/oauth.access?code='
+            +authCode+
+            '&client_id='+process.env.CLIENT_ID+
+            '&client_secret='+process.env.CLIENT_SECRET+
+            '&redirect_uri='+process.env.REDIRECT_URI,
+        method: 'GET'
+    }
+    request(options, (error, response, body) => {
+      var JSONresponse = JSON.parse(body)
+      if (!JSONresponse.ok){
+          reject("Error encountered: \n"+JSON.stringify(JSONresponse))
+      } else {
+        resolve({
+          username: JSONresponse.user.name,
+          slack_id: JSONresponse.user.id,
+        })
+      }
+    })
+  })
+}
+
+
+module.exports = {
+  authorizeWithAuthCode,
+}
