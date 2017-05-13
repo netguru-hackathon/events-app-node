ad6ca3610e408909f1cb623682209340a887e041
Artur Figiel <arturffigiel@gmail.com>
2017-05-13 19:16:47 +0200

    Add new mocked endpoint

--- api/router.js
+++ api/router.js
@@ -14,12 +14,20 @@ export default (app) => {
   });
 
 
-  app.get('/api/events' (req, res) => {
-    res.json({
-      "name": 'WOWOOW',
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis justo non dui tincidunt egestas. Curabitur sed vestibulum arcu. Sed in enim vel augue pulvinar sollicitudin. Sed convallis tristique leo vel aliquam.",
-      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvGBbRtzgNfNaHPP9X28Gj6OQF0l6ZaiqgxJlujX5QsE5g35Or18WijxY3"
-    });
+  app.get('/api/events', (req, res) => {
+    res.json(
+      [
+        {
+          "name": 'WOWOOW',
+          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis justo non dui tincidunt egestas. Curabitur sed vestibulum arcu. Sed in enim vel augue pulvinar sollicitudin. Sed convallis tristique leo vel aliquam.",
+          "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvGBbRtzgNfNaHPP9X28Gj6OQF0l6ZaiqgxJlujX5QsE5g35Or18WijxY3"
+        },
+        {
+          "name": 'WOWOWO2',
+          "description": "convallis justo non dui tincidunt egestas. Curabitur sed vestibulum arcu. Sed in enim vel augue pulvinar sollicitudin. Sed convallis tristique leo vel aliquam.",
+          "image": "http://aurora-awards.com/wp-content/uploads/2017/05/image-image-7.jpg"
+        }
+      ]);
   });
 
   app.get('/slack/auth', SessionController.create);
